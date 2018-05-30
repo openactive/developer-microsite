68b49cf28ecbdbbc8c89e002a606ce90f492e9c7
Chris Thorpe <drchristhorpe@googlemail.com>
2018-05-30 08:59:09 +0100

    Added initial comments

--- app/feed_tools/checker.py
+++ app/feed_tools/checker.py
@@ -2,6 +2,8 @@ import json
 import logging
 import sys
 
+# define some constants
+
 PY3 = sys.version_info[0] == 3
 
 if PY3:
@@ -12,6 +14,7 @@ else:
 model_directory = 'models'
 
 
+# standards are not validated against yet, but should be
 def load_standard(filename):
     if '.json' not in filename:
         filename = filename + '.json'
@@ -21,7 +24,8 @@ def load_standard(filename):
     file = open(filename, 'r')
     return json.loads(file.read())
 
-
+# TODO how to represent permissive field types better
+# model loader
 def load_model_to_test(filename):
     json_representation = {}
     if '.json' not in filename:
@@ -32,6 +36,7 @@ def load_model_to_test(filename):
     filename = model_directory + '/' + filename
     file = open(filename, 'r')
     thisjson = json.loads(file.read())
+    # add in common fields, such as type and id if the model has an id
     json_representation['type'] = {
         'requiredType': 'http://schema.org/Text',
         'required': True,
@@ -45,11 +50,15 @@ def load_model_to_test(filename):
             'requiredType': 'http://schema.org/url',
             'required': True
         }
+    # iterate through the model's fields
     for field in thisjson['fields']:
+        # if a field points at another model, load it
         if 'model' in thisjson['fields'][field]:
             json_representation[field] = load_model_to_test(thisjson['fields'][field]['model'])
+            # set the required type to the model
             json_representation[field]['requiredType'] = thisjson['fields'][field]['model'].replace(
                 '#', '')
+            # TODO consider refactoring this into a function
             try:
                 if field in thisjson['requiredFields']:
                     json_representation[field]['required'] = True
@@ -62,15 +71,19 @@ def load_model_to_test(filename):
             except:
                 json_representation[field]['optional'] = True
         else:
+            # the field does not point to a model, so we'll set up the field definition
+            # remove unused field definition elements
             del thisjson['fields'][field]['example']
             del thisjson['fields'][field]['description']
             del thisjson['fields'][field]['fieldName']
             if 'sameAs' in thisjson['fields'][field]:
                 del thisjson['fields'][field]['sameAs']
+            # special case for @context and the inability of Python to reference it
             if field == '@context':
                 json_representation['context'] = thisjson['fields']['@context']
             else:
                 json_representation[field] = thisjson['fields'][field]
+            # again refactor this into a shared function
             try:
                 if field in thisjson['requiredFields']:
                     json_representation[field]['required'] = True
@@ -82,11 +95,13 @@ def load_model_to_test(filename):
                     json_representation[field]['optional'] = True
             except:
                 json_representation[field]['optional'] = True
+            # load the standard if the field uses one (not validated against yet)
             if 'standard' in field:
                 json_representation[field]['standard'] = load_standard(field['standard'])
     return json_representation
 
-
+# function to check feed field types
+# TODO possibly rename this function
 def check_feed(json_to_check, model_to_test, location_of_sessions=None, array_pointer=None, in_data=False):
 
     if location_of_sessions is not None:
@@ -103,6 +118,8 @@ def check_feed(json_to_check, model_to_test, location_of_sessions=None, array_po
     return forward_errors
 
 
+# function to check feed field presence
+# TODO possibly rename this function
 def check_canonical(json_to_check, model_to_test, location_of_sessions=None, array_pointer=None, in_data=False):
     if location_of_sessions is not None:
         json_to_check = json_to_check[location_of_sessions]
@@ -117,7 +134,9 @@ def check_canonical(json_to_check, model_to_test, location_of_sessions=None, arr
 
     return reverse_errors
 
+### TESTS ###
 
+# test for whether the field value is a URL
 def test_is_url(value, required=True):
     if isinstance(value, string_types):
         if value[0:4] == 'http':
@@ -131,6 +150,7 @@ def test_is_url(value, required=True):
     return errors
 
 
+# test for whether the field is a String
 def test_is_text(value, required=True):
     if isinstance(value, string_types):
         errors = {'success': True, 'message': 'The field is the correct type (String).'}
@@ -140,6 +160,7 @@ def test_is_text(value, required=True):
     return errors
 
 
+# test for whether the field is an Integer
 def test_is_integer(value, required=True):
     if isinstance(value, int):
         errors = {'success': True, 'message': 'The field is the correct type (Integer).'}
@@ -149,6 +170,7 @@ def test_is_integer(value, required=True):
     return errors
 
 
+# test for whether the field is a Float
 def test_is_float(value, required=True):
     if isinstance(value, float):
         errors = {'success': True, 'message': 'The field is the correct type (Float).'}
@@ -157,10 +179,14 @@ def test_is_float(value, required=True):
     return errors
 
 
+# test for whether the field is a datetime
+# TODO fill out test
+# TODO also test for date
 def test_is_datetime(value, required=True):
     return {}
 
 
+# test for whether specific content is present
 def test_content(value, content):
     if value.lower() == content.lower():
         errors = {'success': True, 'message': 'Required content is present'}
@@ -170,6 +196,7 @@ def test_content(value, content):
     return errors
 
 
+# switch for tests
 def test_required_type(fieldname, value, requiredType, standard=False, required=True):
     if requiredType == 'http://schema.org/Text':
         errors = test_is_text(value, required)
@@ -184,10 +211,12 @@ def test_required_type(fieldname, value, requiredType, standard=False, required=
     return errors
 
 
+# TODO use this, at the moment we only test for required types
 def test_preferred_type(fieldname, value, requiredType, standard=False, required=False):
     return test_required_type(fieldname, value, requiredType, standard, required)
 
 
+# testing an individual field
 def test_feed_field(fieldname, value, tests):
     errors = {}
     if fieldname == 'type':
@@ -204,6 +233,8 @@ def test_feed_field(fieldname, value, tests):
     return errors
 
 
+# testing a dictionary node
+# TODO include how to test within arrays
 def test_feed_node(node, testnode):
     errors = {}
     if isinstance(node, dict):
@@ -225,7 +256,7 @@ def test_feed_node(node, testnode):
                                 ' is not yet represented in the Open Active models. '}
     return errors
 
-
+# TODO rename this, we're looking for missing fields
 def test_canonical_node(node, testnode):
     errors = {}
     for item in testnode:
